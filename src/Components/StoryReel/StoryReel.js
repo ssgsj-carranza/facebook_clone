aa778502ba36ff08948cb6a3e4945916e74717c2
unknown <ssgsj.carranza@gmail.com>
2021-06-08 12:14:16 -0500

    created message sender component to post onto the feed imported icons from material ui and working on styling

--- src/Components/StoryReel/StoryReel.js
+++ src/Components/StoryReel/StoryReel.js
@@ -5,7 +5,7 @@ import './StoryReel.css'
 
 function StoryReel() {
     return (
-        <div className='StoryReel'>
+        <div className='storyReel'>
             <Story image='https://images.unsplash.com/photo-1519074069444-1ba4fff66d16?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' profileSrc='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' title='Scarlett Scare'/>
             <Story image='https://images.unsplash.com/photo-1616906852314-79e879ee134f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80' profileSrc='https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' title='John Johnson'/>
             <Story image='https://images.unsplash.com/photo-1616905884459-0fd9f425bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80' profileSrc='https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' title='Fran Fitters'/>
