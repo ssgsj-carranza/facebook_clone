99ec042e4dd5691d4f853f0cd4690af7f4b3c1cd
unknown <ssgsj.carranza@gmail.com>
2021-06-08 22:54:38 -0500

    made posts collection in the back end and connected it to front end, managed to pull test posts onto the the feed

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MessageSender from '../MessageSender/MessageSender'
 import Post from '../Post/Post'
 import StoryReel from '../StoryReel/StoryReel'
 import './Feed.css'
+import db from '../../firebase';
+import {useEffect} from 'react'
 
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+    useEffect (() => {
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map((doc) => ({id: doc.id, data: doc.data()})))
+        ))
+    },[])
     return (
         <div className='feed'>
             <StoryReel />
             <MessageSender />
-            <Post   
-                    profilePic='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-                    message='vacay!!!'
-                    timestamp='time stamp here'
-                    username='scarlet'
-                    image='https://cms.qz.com/wp-content/uploads/2019/07/beachchair.jpg?quality=75&strip=all&w=410&h=240'
-            />
-            <Post
-                    profilePic='https://images.unsplash.com/photo-1616906968825-46cef7c2a508?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80'
-                    message='Like this post'
-                    timestamp='time stamp here'
-                    username='alex'
-            />
+            {posts.map((post) => (
+                <Post
+                    key={post.id}
+                    profilePic={post.data.profilePic}
+                    message={post.data.message}
+                    timestamp={post.data.timestamp}
+                    username={post.data.username}
+                    image={post.data.image}/>
+            ))}
         </div>
     )
 }
